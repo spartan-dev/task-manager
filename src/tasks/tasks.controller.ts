588a3dfefae8113024b0793c0f7accef8d83f27e
Salvador Emmanuel <s.e.jugr@gmail.com>
2024-06-20 21:58:11 -0600

    chore: add my first commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,54 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Query,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-task-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
+import { Task } from './task.entity';
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private tasksService: TasksService) {}
+
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Promise<Task> {
+        return this.tasksService.getTaskById(id);
+    }
+    /* @Get()
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        // filters defined
+        if (Object.keys(filterDto).length) {
+            return this.tasksService.getTasksWithFilters(filterDto);
+        } else {
+            return this.tasksService.getAllTasks();
+        }
+    }
+   
+
+    @Post()
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
+        return this.tasksService.createTask(createTaskDto);
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id: string): void {
+        this.tasksService.deleteTask(id);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id') id: string,
+        @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+    ): Task {
+        const { status } = updateTaskStatusDto;
+        return this.tasksService.updateTaskStatus(id, status);
+    } */
+}
